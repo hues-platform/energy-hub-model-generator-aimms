489b5cffe3074e9154df31e2d718503d9021cc95
Andrew <andrew.bollinger@empa.ch>
2017-03-24 17:25:48 +0100

    updating suurstoffi simple case

--- experiments/suurstoffi_simple/SetupExperiments.m
+++ experiments/suurstoffi_simple/SetupExperiments.m
@@ -1,55 +1,26 @@
 %% DESCRIPTION
 
-% This is a simple example of a script for setting up experiments.  This script creates and executes a single experiment (model run).
-% To execute your experiments, simply run this script.
+% USE THIS SCRIPT TO SET UP AND RUN THE CODE FOR YOUR EXERCISES
+% EACH "%%" MARKS A NEW CODE SECTION.
+% TO RUN A CODE SECTION, PUT OUR CURSOR IN THE RESPECTIVE SECTION AND CLICK "RUN SECTION" ABOVE.
 
-%% CLEAR THE WORKSPACE
+%% 1. CLEAR THE WORKSPACE AND SET THE DIRECTORIES (run this before each exercise)
 
 clear
 clc
-
-%% SET THE DIRECTORIES
-
-%set this to the path of the experiment folder
 experiment_path = 'C:\Users\boa\Documents\Repositories_Github\energy-hub-model-generator-aimms\experiments\generic_energy_hub_basic\';
-
-%set this path to the root of the project
 project_root_path = 'C:\Users\boa\Documents\Repositories_Github\energy-hub-model-generator-aimms\';
-
 cd(project_root_path);
 
-%% ADD DIRECTORIES TO THE MATLAB SEARCH PATH
+%% 2. ADD DIRECTORIES TO THE MATLAB SEARCH PATH (run this once at the start)
 
 addpath(genpath(strcat(project_root_path,'src')));
 addpath(genpath(experiment_path));
 
-%% DEFINE AND RUN THE EXPERIMENTS
-%% Experiment 1 - with technology selection and sizing
+%% 3. RUN THE EXPERIMENT
 
-%set here the values of the parameters that should be varied in your experiments
-%the values of the varied parameters should be given the corresponding variable name in SetExperimentParameters.m.
-
-experiment_name = 'Generic_energy_hub_experiment_with_sizing';
-
-experiment_select_techs_and_do_sizing = 1;
-experiment_include_installed_technologies = 0;
-
-%set the experiment parameters and run the experiment
-SetExperimentParameters
-RunExperiment
-
-%% Experiment 2 - without techonology selection and sizing
-
-experiment_name = 'Generic_energy_hub_experiment_without_sizing';
-
+experiment_name = 'Suurstoffi_simple';
 experiment_select_techs_and_do_sizing = 0;
 experiment_include_installed_technologies = 1;
-
-%set the experiment parameters and run the experiment
 SetExperimentParameters
 RunExperiment
-
-
-%% Experiment n
-
-%add another experiment here...
